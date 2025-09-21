307c4ef98efd15e5ecbcbfaf761c34228a9fadf8
Dayna Pennock <dayna.pennock@student.sl.on.ca>
2025-09-21 18:41:19 -0400

    card alignments look good

--- newest-lab-2/src/App.jsx
+++ newest-lab-2/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <div className = "main-container">
         <Card image="src\assets\images\1.jpg" location = "Indonesia" resort = "Gili Air Hotel" price="$589/night" goodRating = {true} rating = "4.8 ★"/>
         <Card image="src\assets\images\2.jpg" location = "Seychelles" resort = "Hilton Resort" price="$629/night" goodRating = {true} rating = "4.2 ★"/>
-        <Card image="src\assets\images\3.jpg" location = "US Virgin Islands" resort = "Goa Resort" price="$485/night" goodRating =  {false} rating = "3.5 ★"/>
+        <Card image="src\assets\images\3.jpg" location = "Virgin Islands" resort = "Goa Resort" price="$485/night" goodRating =  {false} rating = "3.5 ★"/>
         <Card image="src\assets\images\4.jpg" location = "Bahamas" resort = "Kuredu Resort" price="$729/night" goodRating = {true} rating = "4.1 ★"/>
         <Card image="src\assets\images\5.jpg" location = "Mauritius" resort = "Trou D'eau Douce" price="$877/night" goodRating = {true} rating = "4.9 ★"/>
         <Card image="src\assets\images\6.jpg" location = "Bermuda" resort = "Staniel Cay Hotel" price="$365/night" goodRating = {false} rating = "3.2 ★"/>
