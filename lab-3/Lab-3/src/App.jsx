8b4ff7ced6a527220ba06b7aa7cd1ee445b7f71c
Dayna Pennock <dayna.pennock@student.sl.on.ca>
2025-10-07 21:38:09 -0400

    first-draft no clickable randomization yet

---
+++ lab-3/Lab-3/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ColorBox from './Components/ColorBox';
+import data from "./data/data";
+
+function App() {
+
+  return (
+        <ColorBox colors={data}/>
+  )
+}
+
+export default App
